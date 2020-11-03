68096360918349ec7e543c2224474c9906e95968
Raton Biswas <raton.biswas005@gmail.com>
2020-11-03 14:44:59 +0600

    practice react

---
+++ myapp/src/UserOutput/UserOutput.js
@@ -0,0 +1,10 @@
+
+import React from 'react'
+const userOutput =()=>{
+    return(
+        <div>
+            <p>Some random text</p>
+            <p>I hope i'll be overwritten</p>
+        </div>
+    )
+}
