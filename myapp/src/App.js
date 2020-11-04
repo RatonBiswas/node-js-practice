f123a23aa9be198bb1c712585c5f602917b28a39
Raton Biswas <raton.biswas005@gmail.com>
2020-11-04 10:10:07 +0600

    hide div

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -13,16 +13,18 @@ const App=(props)=> {
       { name: "Nana", age: 22 },
       { name: "Sibli", age: 23 },
     ],
-    otherStates: 'some other state'
+    otherStates: 'some other state',
+    showPersons: false,
   });
   
   const switchNameHandler = (newName) => {
     setPersonsState({
       persons: [
-        { name: 'Raton', age: 21 },
+        { name: newName, age: 21 },
         { name: "Nana", age: 22 },
         { name: "Sibli", age: 2 },
       ],
+      
     });
   };
 
@@ -34,6 +36,11 @@ const App=(props)=> {
         { name: "Sibli", age: 2 },
       ],
     });
+  }
+  const taggleNameHandler = ()=> {
+    const doesShow = personsState.showPersons;
+    // if doesShow was false , it will set showPersons to true .
+    setPersonsState({ showPersons: !doesShow });
   }
   const style = {
     backgroundColor:'white',
@@ -51,22 +58,26 @@ const App=(props)=> {
         <UserOutput userName='raton anna' /> */}
         <h1>Hi, I'm react App.</h1>
         <p>How was the day!</p>
-        <button style={style} onClick={()=> switchNameHandler('lasbone')}>Switch Name</button>
-        <Person 
-          name={personsState.persons[0].name}
-          age={personsState.persons[0].age}
-        />
-        <Person click={switchNameHandler.bind(this,'Lucifer')}
-          changed={nameChangedHandler}
-          name={personsState.persons[1].name}
-          age={personsState.persons[1].age}
-        >
-          My Hobbies: Racing
-        </Person>
-        <Person
-          name={personsState.persons[2].name}
-          age={personsState.persons[2].age}
-        />
+        <button style={style} onClick={taggleNameHandler}>Switch Name</button>
+        { 
+          personsState.showPersons === true ?
+            <div>
+              <Person 
+                name={personsState.persons[0].name}
+                age={personsState.persons[0].age}
+              />
+              <Person click={switchNameHandler.bind(this,'Lucifer')}
+                changed={nameChangedHandler}
+                name={personsState.persons[1].name}
+                age={personsState.persons[1].age}
+              >
+                My Hobbies: Racing
+              </Person>
+              <Person
+                name={personsState.persons[2].name}
+                age={personsState.persons[2].age}
+              />
+            </div> : null}
       </div>
     );
   }
